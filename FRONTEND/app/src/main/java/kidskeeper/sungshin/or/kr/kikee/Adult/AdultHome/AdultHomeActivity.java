c61948445ca33bb3483f6dcb27badf4ea72f3a26
jieun <jieun3001@gmail.com>
2018-07-05 16:52:46 +0900

    안드로이드 어른 레이아웃 추가

---
+++ FRONTEND/app/src/main/java/kidskeeper/sungshin/or/kr/kikee/Adult/AdultHome/AdultHomeActivity.java
@@ -0,0 +1,15 @@
+package kidskeeper.sungshin.or.kr.kikee.Adult.AdultHome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kidskeeper.sungshin.or.kr.kikee.R;
+
+public class AdultHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adult_home);
+    }
+}
--- FRONTEND/app/src/main/java/kidskeeper/sungshin/or/kr/kikee/AdultLoginActivity.java
+++ FRONTEND/app/src/main/java/kidskeeper/sungshin/or/kr/kikee/AdultLoginActivity.java
@@ -1,13 +1,54 @@
 package kidskeeper.sungshin.or.kr.kikee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import kidskeeper.sungshin.or.kr.kikee.Adult.AdultHome.AdultHomeActivity;
 
 public class AdultLoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.adultlogin_edittext_id)
+    EditText editTextId;
+    @BindView(R.id.adultlogin_edittext_pw)
+    EditText editTextPw;
+    @BindView(R.id.adultlogin_button_login)
+    Button buttonLogin;
+    @BindView(R.id.adultlogin_button_back)
+    Button buttonback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adult_login);
+        ButterKnife.bind(this);
+        clickEvent();
+    }
+
+    void clickEvent() {
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AdultHomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentBack = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intentBack);
+                finish();
+
+            }
+        });
     }
 }
