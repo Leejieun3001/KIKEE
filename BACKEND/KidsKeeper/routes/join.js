7c9b159c396e34e1298600f0be9c2fc2fedca492
jieun <jieun3001@gmail.com>
2018-07-11 15:54:30 +0900

    서버  회원가입 메일 인증 api 구현

--- BACKEND/KidsKeeper/routes/join.js
+++ BACKEND/KidsKeeper/routes/join.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const async = require('async');
 const globalModule = require('../module/globalModule');
 const nodemailer = require('nodemailer');
+const mailConfig = require('../config/mailAccount');
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
@@ -123,4 +124,107 @@ router.get('/check_iotNumber', function (req, res) {
     var task = [globalModule.connect.bind(this), checkID, globalModule.releaseConnection.bind(this)];
     async.waterfall(task, globalModule.asyncCallback.bind(this));
 });
+
+/**
+ * api 목적        : 아이디 중복체크
+ * request params : { string id: "아이디" }
+ */
+router.get('/check_dupplicate', function (req, res) {
+    let checkValid = function (connection, callback) {
+        let result = globalModule.checkBasicValid(req.query);
+
+        if (result !== "OK") {
+            res.status(200).send(result);
+            callback("ALREADY_SEND_MESSAGE", connection, "api : /join/check_dupplicate");
+        } else {
+            callback(null, connection);
+        }
+    }
+
+    let checkID = function (connection, callback) {
+        connection.query('select * from User where id = ?', req.query.id, function (error, rows) {
+            if (error) {
+                callback(error, connection, "Select query Error : ", res);
+            } else {
+                if (rows.length !== 0) {
+                    res.status(200).send(errorConfig.ALREADY_JOIN);
+                    callback("ALREADY_SEND_MESSAGE", connection, "api : /join/check_dupplicate");
+                } else {
+                    res.status(200).send({ message: "SUCCESS" });
+                    callback(null, connection, "api : /join/check_dupplicate");
+                }
+            }
+        });
+    }
+
+    var task = [globalModule.connect.bind(this), checkValid, checkID, globalModule.releaseConnection.bind(this)];
+    async.waterfall(task, globalModule.asyncCallback.bind(this));
+});
+
+/**
+ * api 목적        : email 인증
+ * request params : tempEmail(query)
+ */
+
+router.get('/verificationCode', function (req, res) {
+
+    var Transport = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: mailConfig.jieun.user,
+            pass: mailConfig.jieun.pass
+        }
+    });
+
+    var rand;
+    var resultJson = {
+        message: '',
+        verificationCode: ''
+    };
+
+    var selectUserInfo = function (connection, callback) {
+        let duplicate_check_query = "select * from User where email = ?";
+        connection.query(duplicate_check_query, req.query.tempEmail, function (err, data) {
+            if (err) {
+                console.log("duplicate check select query error : ", err);
+                callback(err, connection, "api : /join/verificationCode");
+            } else {
+                if (data.length == 0) {
+                    callback(null, connection);
+                }
+                else {
+                    res.status(201).send(errorConfig.ALREADY_JOIN);
+                    callback("ALREADY_SEND_MESSAGE", connection, "api : /join/verificationCode");
+                }
+            }
+        });
+    }
+
+    var sendMail = function (connection, callback) {
+        rand = Math.floor((Math.random() * 10000));
+        let mailOption = {
+            to: req.query.tempEmail,
+            subject: "안녕하세요. KIKEE 입니다.",
+            html: "안녕하세요,<br> 고객님의 인증번호는 " + rand + "입니다. <br>"
+                + "<br>어플로 돌아가셔서 인증번호를 입력해 주세요.</br>"
+                + "<br>감사합니다.</br>"
+        };
+        Transport.sendMail(mailOption, function (error, info) {
+            if (error) {
+                callback(error, connection, "Transport Error : ");
+            } else {
+                resultJson.message = "SUCCESS";
+                resultJson.verificationCode = rand;
+                res.status(201).send(resultJson);
+                console.log(1);
+                callback(null, connection, "api : /join/verificationCode", res);
+            }
+        });
+    }
+
+    var verificationCode_task = [globalModule.connect.bind(this), selectUserInfo, sendMail, globalModule.releaseConnection.bind(this)];
+    async.waterfall(verificationCode_task, globalModule.asyncCallback.bind(this));
+});
+
+
 module.exports = router;
